71af2f5e0b8c630938fde9fc583f2543aebcb53f
2Mw <abd771567@gmail.com>
2023-10-31 21:57:25 +0800

    feat: add google map button and strip exif info.

--- pages/exif-tools.vue
+++ pages/exif-tools.vue
@@ -18,12 +18,19 @@
             </div>
             <img :src="imgURL" class="absolute self-center h-full brightness-50" />
         </div>
-        <p class="text-sm text-center text-slate-400 my-1">{{ imgName }}</p>
+        <p class="text-sm text-center text-slate-400 my-2" v-if="imgName.length > 0">{{ imgName }}</p>
+        <p class="text-sm text-center text-slate-400 my-2" v-if="imgName.length > 0">
+            <UButton variant="outline" size="xs" class="text-xs" @click="clearExif">Clear EXIF</UButton>
+            <a :href="gpsLink" v-if="gpsLink.length > 0" target="_blank" class="ml-3">
+                <UButton variant="outline" color="red" size="xs">This Position!!</UButton>
+            </a>
+        </p>
         <Hr />
         <div class="grid lg:grid-cols-3 gap-6 grid-cols-1">
             <EntryTable v-if="basicInfoTable.length > 0" caption="Basic Info" :rows="basicInfoTable" />
             <EntryTable v-if="deviceInfoTable.length > 0" caption="Device Info" :rows="deviceInfoTable" />
             <EntryTable v-if="exifInfoTable.length > 0" caption="Exif Info" :rows="exifInfoTable" />
+            <EntryTable v-if="gpsInfoTable.length > 0" caption="GPS Info" :rows="gpsInfoTable" />
             <EntryTable v-if="ifd1InfoTable.length > 0" caption="ifd1 Info" :rows="ifd1InfoTable" />
             <EntryTable v-if="iccInfoTable.length > 0" caption="ICC Info" :rows="iccInfoTable" />
             <EntryTable v-if="jijfInfoTable.length > 0" caption="JIJF Info" :rows="jijfInfoTable" />
@@ -44,8 +51,17 @@ import exifr, { Exifr } from 'exifr';
 import { Entry } from '~/composables/models/Entry';
 const exifText = "Exchangeable image file format，包含数码照片的属性信息和拍摄数据。"
 
+useHead({
+    title: 'Exif Tool'
+})
+
+//  GoogleMap: https://www.google.com/maps/@($latitude),($longtitude44),15z
+
 const imgURL = ref('');
 const imgName = ref('');
+const imgFile = ref({} as File);
+
+const gpsLink = ref('');
 
 const defaultOptions = {
     // Segments (JPEG APP Segment, PNG Chunks, HEIC Boxes, etc...)
@@ -78,6 +94,8 @@ let mergeInfo = ref({} as any)
 let basicInfoTable = ref([] as Entry[])
 // 设备信息
 let deviceInfoTable = ref([] as Entry[]);
+// GPS信息
+let gpsInfoTable = ref([] as Entry[]);
 // Exif信息
 let exifInfoTable = ref([] as Entry[]);
 // ICC 信息
@@ -103,9 +121,9 @@ watch(mergeInfo, () => {
     let sizeEntry = new Entry('ImageSize', '');
     let flag = true;
     if (mergeInfo.value.hasOwnProperty('ExifImageHeight')) sizeEntry.value = `${mergeInfo.value['ExifImageWidth']} x ${mergeInfo.value['ExifImageHeight']}`;
-    else if(mergeInfo.value.hasOwnProperty('ImageWidth')) sizeEntry.value = `${mergeInfo.value['ImageWidth']} x ${mergeInfo.value['ImageHeight']} `;
+    else if (mergeInfo.value.hasOwnProperty('ImageWidth')) sizeEntry.value = `${mergeInfo.value['ImageWidth']} x ${mergeInfo.value['ImageHeight']} `;
     else flag = false;
-    if(flag)basicInfoTable.value.push(sizeEntry);
+    if (flag) basicInfoTable.value.push(sizeEntry);
 
     for (let key of basicInfoKeys) {
         if (mergeInfo.value.hasOwnProperty(key)) {
@@ -116,8 +134,6 @@ watch(mergeInfo, () => {
     // raw
     rawInfoTable.value.splice(0, rawInfoTable.value.length);
     for (let key in mergeInfo.value) {
-        console.log(key);
-
         rawInfoTable.value.push(new Entry(key, mergeInfo.value[key]));
     }
 });
@@ -139,6 +155,21 @@ watch(rawInfo, () => {
 
         for (let key in exifObj) {
             exifInfoTable.value.push(new Entry(key, exifObj[key]));
+        }
+    }
+
+    // exif
+    gpsInfoTable.value.splice(0, gpsInfoTable.value.length);
+    gpsLink.value = '';
+    if (rawInfo.value.hasOwnProperty('gps')) {
+        let gps = rawInfo.value['gps'];
+
+        for (let key in gps) {
+            gpsInfoTable.value.push(new Entry(key, gps[key]));
+        }
+
+        if (gps.hasOwnProperty('latitude')) {
+            gpsLink.value = `https://www.google.com/maps/@${gps['latitude']},${gps['longitude']},15z`
         }
     }
 
@@ -182,20 +213,70 @@ watch(rawInfo, () => {
 });
 
 const submitImg = async (e: any) => {
-    let filename: File = e.target.files[0];
-    if (filename) {
-        imgName.value = filename.name;
+    imgFile.value = e.target.files[0];
+    if (imgFile.value) {
+        imgName.value = imgFile.value.name;
         const reader = new FileReader();
         reader.addEventListener('load', () => {
             if (typeof reader.result === 'string') imgURL.value = reader.result;
         })
-        reader.readAsDataURL(filename);
-        rawInfo.value = await exifr.parse(filename, defaultOptions);
-        mergeInfo.value = await exifr.parse(filename, { ...defaultOptions, mergeOutput: true });
+        reader.readAsDataURL(imgFile.value);
+        rawInfo.value = await exifr.parse(imgFile.value, defaultOptions);
+        mergeInfo.value = await exifr.parse(imgFile.value, { ...defaultOptions, mergeOutput: true });
 
-        console.log('rawinfo:', rawInfo.value);
-        console.log('mergeInfo:', mergeInfo.value);
+        // console.log('rawinfo:', rawInfo.value);
+        // console.log('mergeInfo:', mergeInfo.value);
     }
+}
+
+
+const clearExif = async () => {
+    const reader = new FileReader();
+
+    // refer: https://jsfiddle.net/mowglisanu/frhwm2xe/3/
+    reader.addEventListener('load', () => {
+        if (reader.result != null && reader.result instanceof ArrayBuffer) {
+            var dv = new DataView(reader.result);
+            var offset = 0, recess = 0;
+            var pieces = [];
+            var i = 0;
+            if (dv.getUint16(offset) == 0xffd8) {
+                offset += 2;
+                var app1 = dv.getUint16(offset);
+                offset += 2;
+                while (offset < dv.byteLength) {
+                    if (app1 == 0xffe1 || app1 == 0xffe2) {
+                        pieces[i] = { recess: recess, offset: offset - 2 };
+                        recess = offset + dv.getUint16(offset);
+                        i++;
+                    }
+                    else if (app1 == 0xffda) {
+                        break;
+                    }
+                    offset += dv.getUint16(offset);
+                    var app1 = dv.getUint16(offset);
+                    offset += 2;
+                }
+                if (pieces.length > 0) {
+                    var newPieces = [];
+                    pieces.forEach(function (v) {
+                        newPieces.push(reader.result?.slice(v.recess, v.offset));
+                    }, this);
+                    newPieces.push(reader.result.slice(recess));
+                    var br = new Blob(newPieces, { type: 'image/jpeg' });
+                    let clearedURL = URL.createObjectURL(br)
+                    console.log(clearedURL);
+
+                    var a = document.createElement('a');
+                    a.href = clearedURL;
+                    a.download = crypto.randomUUID();
+                    a.click();
+                }
+            }
+        }
+    });
+
+    reader.readAsArrayBuffer(imgFile.value);
 }
 
 </script>
